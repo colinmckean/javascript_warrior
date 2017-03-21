438f6a0058e9d705c7861b8d303fe9f6a728e756
Colin McKean <colinmckeandigital@gmail.com>
2017-03-21 15:38:20 +0000

    pass greataxe test

--- specs/barbarian_specs.js
+++ specs/barbarian_specs.js
@@ -5,4 +5,10 @@ describe('Barbarian', function(){
   it('should have a name of Maximus!', function(){
     assert.equal("Maximus", barbarian.name);
   });
+  it('should have a weapon with a name of greatAxe',
+    function(){
+      assert.equal("Great Axe", barbarian.weapon.name);
+    });
+
+
 });
