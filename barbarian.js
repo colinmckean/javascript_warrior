3a06eee3a586ec630e207cdd7a4ac81e1b4cac88
Colin McKean <colinmckeandigital@gmail.com>
2017-03-21 15:38:41 +0000

    pass anger test

--- barbarian.js
+++ barbarian.js
@@ -1,5 +1,6 @@
 var barbarian = {
   name: "Maximus",
+  angerLevel: 0,
   weapon: greatAxe = {name: "Great Axe", damage: 10}
 }
 module.exports = barbarian;
--- specs/barbarian_specs.js
+++ specs/barbarian_specs.js
@@ -9,6 +9,9 @@ describe('Barbarian', function(){
     function(){
       assert.equal("Great Axe", barbarian.weapon.name);
     });
-
+  it('should have an anger level that starts at 0 because he is pretty chilled.', function(){
+    assert.equal(0, barbarian.angerLevel);
+  });
+  
 
 });
