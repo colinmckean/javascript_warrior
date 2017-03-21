dee28f72d5d3e4185efe9fe18b2021201d383dae
Colin McKean <colinmckeandigital@gmail.com>
2017-03-21 20:40:08 +0000

    add test for plasma cannon charge

--- specs/plasma_cannon_specs.js
+++ specs/plasma_cannon_specs.js
@@ -3,7 +3,13 @@ var barbarian = require('../barbarian');
 var pm = require('../plasma_cannon');
 
 describe('Plasma Canon', function () {
+  beforeEach(function() {
+    pm.charge = 0;
+  });
   it('should have a name of plasma cannon 2000!', function () {
     assert.equal("Plasmah Cannon 2000", pm.name);
+  });
+  it('should have a charge level of 0 to start with', function (){
+    assert.equal(0, pm.charge);
   });
 });
