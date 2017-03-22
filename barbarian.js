42ad6ca55935d595947d4d14535063a7f6c64e8e
Colin McKean <colinmckeandigital@gmail.com>
2017-03-22 08:58:21 +0000

    spelling fix

--- barbarian.js
+++ barbarian.js
@@ -12,7 +12,7 @@ var barbarian = {
   attack: function () {
     return this.name + this.weapon.action();
   },
-  backstory: "Maximus would spend his days writing backstories for barbarians, until.... one day he found that there was fracture in the space time continuum. Barbarians from other dimentions appeared through this breach, they would claim to be named Maximus however they would have their own backstory putting Maximus out of business. From this day he vowed to eliminate these shallow copies with his plasma cannon.",
+  backstory: "Maximus would spend his days writing backstories for barbarians, until.... one day he found that there was fracture in the space time continuum. Barbarians from other dimensions appeared through this breach, they would claim to be named Maximus however they would have their own backstory putting Maximus out of business. From this day he vowed to eliminate these shallow copies with his plasma cannon.",
   weapon: { name: "Great Axe", damage: 10, action: function () {
     return " swings"  + " his " + this.name + " dealing " + this.damage + " damage";
   }
